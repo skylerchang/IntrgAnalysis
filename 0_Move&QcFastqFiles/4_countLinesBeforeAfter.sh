60296510eb82fb9b1feba03da3507b280f8f5b63
Stefan Keller <smkeller@uoguelph.ca>
2019-01-21 00:52:40 -0500

    Create 4_countLinesBeforeAfter.sh

---
+++ 0_Move&QcFastqFiles/4_countLinesBeforeAfter.sh
@@ -0,0 +1,29 @@
+targetFolder="../../Data"
+destination=$targetFolder"/Trimmed30"
+
+mkdir -p $targetFolder"/Counts/"
+
+touch $targetFolder"/Counts/before-names.txt"
+touch $targetFolder"/Counts/before-count.txt"
+touch $targetFolder"/Counts/after-names.txt"
+touch $targetFolder"/Counts/after-count.txt"
+
+for d in $targetFolder/"Original/"* 
+do 
+	echo "d: "$d
+	echo $d >> $targetFolder"/Counts/before-names.txt"
+	for f in $d/*
+	do
+		echo "f: "$f
+		zgrep -Ec "$" $f >> $targetFolder"/Counts/before-count.txt"
+	done 
+done
+
+for f in $targetFolder/"Trimmed30/Paired/"* 
+do 
+	echo "f: "$f
+	echo $f >> $targetFolder"/Counts/after-names.txt"
+	zgrep -Ec "$" $f >> $targetFolder"/Counts/after-count.txt"
+
+done
+
