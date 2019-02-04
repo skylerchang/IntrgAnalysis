e2467abc2f8795da121e63ca9c4eaf1e1d1c0112
Stefan Keller <smkeller@uoguelph.ca>
2019-02-03 23:23:57 -0500

    moveFastq changed to combine files in one folder

--- 0_Move&QcFastqFiles/0b_moveFastq_AfterBasespaceDownload.sh
+++ 0_Move&QcFastqFiles/0b_moveFastq_AfterBasespaceDownload.sh
@@ -12,18 +12,19 @@ do
 	#get first part of foldername for part1 of final file name
 	dirname="${d##*/}"
 	echo "dirname: "$dirname
-	part1=$(echo $dirname | perl -lne 'print $1 if /(.*)_L001/')
+	part1=$(echo $dirname | perl -lne 'print $1 if /(.*_)L001/')
 	echo "part1: "$part1
-	mkdir $targetFolder"Original/"$part1
+#	mkdir $targetFolder"Original/"$part1
 	
  	for f in $d/*
  	do
  		echo $f
  		filename="${f##*/}"
- 		part2=$(echo $filename | perl -lne 'print $1 if /(_L001.*)/')
+# 		part2=$(echo $filename | perl -lne 'print $1 if /(_L001.*)_001.fastq.gz/')
+		part2=$filename
 		echo "part2: "$part2
 		echo "finalFileName: "$part1$part2
-		mv $f $targetFolder"/Original/"$part1"/"$part1$part2
+		mv $f $targetFolder"/Original/"$part1$part2
  	done
  	rmdir $d
 done
