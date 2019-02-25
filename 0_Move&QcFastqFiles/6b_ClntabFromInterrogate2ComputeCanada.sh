dd61af500260f21d1d63b23cba87c3266cc15f37
Stefan Keller <smkeller@uoguelph.ca>
2019-02-24 19:47:54 -0500

    6b_ClntabFromInterrogate2ComputeCanada
    changed to collect all clntab files in folder on Interrogate -> tar -> move -> delete Clntab directory

---
+++ 0_Move&QcFastqFiles/6b_ClntabFromInterrogate2ComputeCanada.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#cd to equivalent of '/intrg_storage/users/smkeller/analyses/Run02_feTR_normal_Q30L50/last_run_res/Run02_feTR_normal--results'
+
+#create folder in which to copy all clntab files
+mkdir ../Clntab
+#move clntab files to folder
+for d in *001/; do echo $d; echo $d"/"*".clntab"; cp $d"/"*".clntab" ../Clntab/; done
+#compress folder
+tar -czvf ../Clntab.tar.gz ../Clntab
+
+#********** transfer tar ball to compute canada (from compute canada) ***************
+rsync -ave ssh keeper@s0.arrest.tools:/intrg_storage/users/smkeller/analyses/Run02_feTR_normal_Q30L50/last_run_res/Clntab.tar.gz ./
+
+
+#clean-up
+rm -rf ../Clntab
