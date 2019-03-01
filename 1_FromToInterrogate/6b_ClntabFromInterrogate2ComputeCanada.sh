4d5650bdc1db93010a670fadd3bcd4169ad173c9
Stefan Keller <smkeller@uoguelph.ca>
2019-02-28 21:36:14 -0500

    Update 6b_ClntabFromInterrogate2ComputeCanada.sh
    Included 'ClntabPlus' transfer, i.e. transfer of additional files beyond clntab

--- 1_FromToInterrogate/6b_ClntabFromInterrogate2ComputeCanada.sh
+++ 1_FromToInterrogate/6b_ClntabFromInterrogate2ComputeCanada.sh
@@ -9,9 +9,26 @@ for d in *001/; do echo $d; echo $d"/"*".clntab"; cp $d"/"*".clntab" ../Clntab/;
 #compress folder
 tar -czvf ../Clntab.tar.gz ../Clntab
 
-#********** transfer tar ball to compute canada (from compute canada) ***************
+#********** transfer tar ball to compute canada (from compute canada or local) ***************
 rsync -ave ssh keeper@s0.arrest.tools:/intrg_storage/users/smkeller/analyses/Run02_feTR_normal_Q30L50/last_run_res/Clntab.tar.gz ./
 
 
 #clean-up
 rm -rf ../Clntab
+
+
+#============== option 2: transfer the entire "--results" folder 
+
+cd /intrg_storage/users/smkeller/analyses/Run02_feTR_normal_Q30L50/last_run_res
+tar -czvf Clntab.tar.gz Run02_feTR_normal_Q30L50--results/
+
+#********** transfer tar ball to compute canada (from compute canada or local) ***************
+rsync -ave ssh keeper@s0.arrest.tools:/intrg_storage/users/smkeller/analyses/Run02_feTR_normal_Q30L50/last_run_res/Clntab.tar.gz ./
+
+tar -xzvf Clntab.tar.gz 
+mv Run02_feTR_normal_Q30L50--results/ ClntabPlus/
+
+#create folder in which to copy all clntab files
+mkdir ../Clntab
+#move clntab files to folder
+for d in *001/; do echo $d; echo $d"/"*".clntab"; cp $d"/"*".clntab" ../Clntab/; done
