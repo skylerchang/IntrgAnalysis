aa63c042addf839a590fee90381657eb7518bef1
Haiyang Chang <35077981+skylerchang@users.noreply.github.com>
2019-04-03 10:37:03 -0400

    Update 1_qcOriginal_separateFolders.sh

--- 0_preClntab/1_qcOriginal_separateFolders.sh
+++ 0_preClntab/1_qcOriginal_separateFolders.sh
@@ -5,6 +5,15 @@
 #pip install --user multiqc
 #I don't know if this is open to other users and if computecanada remembers for next time.
 
+
+###### Computecanada doesn't remember everytime, softwares need to be loaded in every new folder #### 
+module load fastqc
+module load python/3.6.3
+pip install --user multiqc
+mkdir ../../Results
+
+
+
 #once installed, go from here
 targetDir="../../"
 
