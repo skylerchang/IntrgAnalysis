121ef76d0bd653525e0a83030f907fa0ea8499a0
Stefan Keller <smkeller@uoguelph.ca>
2019-04-06 01:39:45 -0400

    Update 7a_analyzeInterrogateRunReport.R
    started adapting the script to make it more flexible for different input sequences (e.g. fractions indicated by PcrId). Revisit & finish.

--- 1_FromToInterrogate/7a_analyzeInterrogateRunReport.R
+++ 1_FromToInterrogate/7a_analyzeInterrogateRunReport.R
@@ -5,22 +5,65 @@
 #-> only one underline; hyphens can occur more frequently -> don't use for regex search 
 #18-010899-1D1P2_Piach-Mowgli_WGA+"
 
+#Naming convention:
+#pcrId, format: "15-013425-1D1P4", controls: "D16-07-1FD1P218", "NTC-can-0D1P164"
+#sampleId: "k9-pc-st_D16-07.2F", k9-pc_D16-07.2F
+#submissionId, format: "15-013425", controls: "D16-07", "NTC-can"
+
 library(tidyverse)
 library(here)
 library(RColorBrewer)
-library(gridExtra)
 library(reshape2)
 library(gsubfn)
+library(gridExtra)
 
-targetDir<-'../../Data/InterrogateRunReport/'
+
+#*************************** adjust settings ************************
+#Is SampleNo coding? values: T/F; (e.g. MRD - yes: jake-01.1 (pbmcs), jake-01.2 (plasma); Histiocytoma - no)
+sampleNoCoding<-0
+
+#********************************************************************
+
+targetDir<-'../../Results/InterrogateRunReport/'
 
 files<-list.files(targetDir,pattern = '.basic--run_report.xlsx')
-
+j<-1
 
 for (j in 1:length(files)){
   t<-readxl::read_excel(paste0(targetDir,files[j]))
   
+  #************** Run-specific code ***************
+  #cleans-up inconsistencies in sample naming 
+  #consider going back and fixing names in the Illumina sample sheet to make this section redundant
+  #************** Run 19 - Histiocytoma ***************
+  t$sample<-sub("D16-07","pc-control",t$sample)
+  t$sample<-sub("NTC-can","nt-control",t$sample)
+  
+  
+  #************** create additional sample designations ***************
+  #simplify sample name -> omit '_S62_L001_R1_001'
+  t$sample<-sub("_S[0-9]+_L001_R1_001","",t$sample)
+  #create PcrId, format: "15-013425-1D1P4", controls: "D16-07-1FD1P218", "NTC-can-0D1P164"
+  t$pcrId<-unlist(strapply(t$sample,"^(.*?)_.*$"))
+  #create sampleId
+  t$sampleId<-unlist(strapply(t$pcrId,"(.*)D[0-9]+P[0-9]+$"))
+  #create submissionId, format: "15-013425", controls: "D16-07", "NTC-can"
+  t$submissionId<-unlist(strapply(t$pcrId,"^(.*)-.*?$"))
+  t$owner.patient<-unlist(strapply(t$sample,"^.*?_(.*)$"))
+  #create sample fraction (MRD study: fraction1: PBMCs, fraction2: plasma; CSF study ...)
+  if (sampleNoCoding){
+    t$sampleFraction<-ifelse(grepl("-1D[0-9]+P[0-9]",t$pcrId),'fraction1','fraction2')
+  }else{
+    t$sampleFraction<-'fraction1'
+  }
+  t$isControl<-ifelse(grepl("control",t$submissionId),T,F)
+  
+  colorCount.sample<-length(unique(t$sampleId))
+  getPalette<-colorRampPalette(brewer.pal(9,'Set1'))
+  
+  
   #*********** Run 26 specific code *************
+  #this might have to be revised based on changes in sample names -> revisit
   #remove Conner's IGL samples
   t<-t[!grepl("IGL",t$sample),]
   
@@ -29,7 +72,7 @@ for (j in 1:length(files)){
   t$sample<-sub("-WGA_","_",t$sample)
   t$sample<-sub("-WGA","_WGA",t$sample)
   
-  #***********************************************
+  #***************** general stuff ******************************
   
   #clean up col names; remove white space and special characters
   colnames(t)<-gsub(" ","_",colnames(t))
@@ -68,28 +111,28 @@ for (j in 1:length(files)){
   t$w_G_chain.count<-as.numeric(t$w_G_chain.count)
   t$w_H_chain.count<-as.numeric(t$w_H_chain.count)
   t$w_K_chain.count<-as.numeric(t$w_K_chain.count)
-  
-  #define additional columns
-  t$owner.patient<-strapply(t$sample, "^.*?_(.*)",simplify=T,perl=T)
-  t$submission<-strapply(t$sample, "^(.*?)_",simplify=T,perl=T)
-  
-  t$sampleType<-ifelse(grepl("-1D[0-9]+P[0-9]+C[0-9]+L[0-9]+P[0-9]+",t$submission),'pellet','supernatant')
-  t$sampleType[grepl("k9",t$submission)]<-'control'
-  t$wga<-ifelse(grepl("WGA",t$submission),TRUE,FALSE)
-  
-  
-  ggplot(t,aes(sample,raw.count))+geom_col()+coord_flip()
-  ggplot(t,aes(sample,w_junction.percent,fill=sampleType))+geom_col()+coord_flip()+theme(axis.text.x = element_text(angle = 90, hjust = 1))
-  
-  
-  
-  
-  #boxplots diversity
+  t$diversity_normed.count<-as.numeric(t$diversity_normed.count)
+  t$alpha_diversity.count<-as.numeric(t$alpha_diversity.count)
+  t$effective_species.count<-as.numeric(t$effective_species.count)
+
+  #============= plot read numbers =============
   plotlist<-list()
-  x<-c('w_junction.percent', 'diversity_normed.count','alpha_diversity.count','effective_species.count')
+  x<-c('raw.count','raw.percent','w_junction.percent', 'w_junction.count')
+  for (i in 1:length(x)){
+    p<-ggplot(t,aes_string("sample",x[i]))+geom_boxplot()+coord_flip()
+    plotlist[[i]]<-ggplotGrob(p)
+  }
+  pdf(paste0(targetDir,'RunReportPlots_readNos.pdf'))
+  marrangeGrob(plotlist,nrow=2,ncol=2)
+  dev.off()
+  
+  #============= plot diversity =============
+  t$sampleId<-as.factor(t$sampleId)
+  plotlist<-list()
+  x<-c('diversity_normed.count','alpha_diversity.count','effective_species.count')
   for (i in 1:length(x)){
     #pdf(paste0(x[i],'.pdf'))
-    p<-ggplot(t[t$sampleType!='control',],aes_string(x="wga",y=x[i],fill="sampleType"))+geom_boxplot(outlier.size = 0)+geom_point(pch = 21,position = position_jitterdodge(jitter.width = 0.2))+scale_fill_manual(values=c('gray80','dodgerblue3'))
+    p<-ggplot(t,aes_string("sampleId",x[i]))+geom_boxplot()+coord_flip()
     p<-p+ggtitle(x[i])+xlab('WGA')+theme(plot.title = element_text(hjust = 0.5))
     plotlist[[i]] <- ggplotGrob(p)
     #dev.off()
@@ -98,7 +141,7 @@ for (j in 1:length(files)){
   marrangeGrob(plotlist,nrow=2,ncol=2)
   dev.off()
   
-  #boxplot by priming
+  #============= plot priming =============
   plotlist<-list()
   x<-c('Five_primed_in_R1.percent',
        'Five_primed_in_R2.percent',
@@ -114,7 +157,7 @@ for (j in 1:length(files)){
   marrangeGrob(plotlist,nrow=2,ncol=2)
   dev.off()
   
-  #boxplot primer dimers
+  #============= primer dimers=============
   plotlist<-list()
   x<-c('w_primerdimers_in_R1.percent','w_primerdimers_in_R2.percent','w_primerdimers_in_R1.count','w_primerdimers_in_R2.count')
   titles<-c("Primer dimers in R1 - percent","Primer dimers in R2 - percent","Primer dimers in R1 - count","Primer dimers in R2 - count")
@@ -127,7 +170,7 @@ for (j in 1:length(files)){
   marrangeGrob(plotlist,nrow=2,ncol=2)
   dev.off()
   
-  #boxplot joining
+  #============= joining ============= 
   plotlist<-list()
   x<-c("joined_normally.count","joined_normally.percent",      
         "joined_loosely.count","joined_loosely.percent",       
