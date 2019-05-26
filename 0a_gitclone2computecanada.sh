f4dd5270811b303389e994d6eaa03f4df01e8244
Haiyang Chang <35077981+skylerchang@users.noreply.github.com>
2019-05-26 19:26:45 -0400

    Delete 0a_gitclone2computecanada.sh

--- 0a_gitclone2computecanada.sh
+++
@@ -1,7 +0,0 @@
-#### Login compute canada account's Shared/SequencingData/ folder and create RunID folder #######
-git clone https://github.com/theKellerLab/IntrgAnalysis
-mkdir Basespace
-mv ds* Basespace/
-mkdir Data
-mv Basespace/ Data/
-cd IntrgAnalysis/0_preClntab
