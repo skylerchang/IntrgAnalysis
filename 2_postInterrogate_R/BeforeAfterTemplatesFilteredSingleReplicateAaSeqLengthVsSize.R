849fac5d86b25135067dbcb8322f224e921f3036
Haiyang Chang <35077981+skylerchang@users.noreply.github.com>
2019-06-17 19:45:12 -0400

    Update and rename CompareTemplatesVsNotOnEachSample.R to BeforeAfterTemplatesFilteredSingleReplicateAaSeqLengthVsSize.R

--- 2_postInterrogate_R/BeforeAfterTemplatesFilteredSingleReplicateAaSeqLengthVsSize.R
+++ 2_postInterrogate_R/BeforeAfterTemplatesFilteredSingleReplicateAaSeqLengthVsSize.R
@@ -21,10 +21,12 @@ col<-c(col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11)
 col<-unique(col)
 
 
+setwd(here())
+getwd()
+
 #========== adjust the following variables ================
-t<-read_rds('RDS/clntab_vAndJ.rds')
-outpath<-'OUT/cdrAaLengthTemplate'
-#loci<-c("TRB","IGH")
+t<-read_rds('../Clntab_RDS/clntab_vAndJ.rds')
+outpath<-'../Results/CdrAaLengthTemplate/'
 loci<-c("IGH")
 #Top n clones that are being displayed in separate colors (all other clones are grey)
 n<-100
@@ -35,19 +37,33 @@ datalist<-t[[1]]
 #sample names are stored separately in a vector
 files_short<-t[[2]]
 
+dir.create(outpath<-'../Results/CdrAaLengthTemplate/',recursive=T)
+
 for (i in 1:length(datalist)){
-  a<-datalist[[i]][,c("vGene","jGene","aaSeq","aaLength","size","completeNtSeq","vAndJchainSimplified")]
-  #####Akash‘s TEMPLATES#######
-  #d <- subset(a(!grepl(".TGTGCATCACGACACAGTGGTCTGG.*",a$completeNtSeq)) | a$aaLength!=？)
-  #d <- subset(d,(!grepl(".TGTGCATCACGACCAGATCCACAGATCCATTGGTTACTGG.*",d$completeNtSeq)) | d$aaLength!=？)
-  #####Tamara‘s TEMPLATES#######
-  d <- subset(a,(!grepl(".TGTTCGCCTTATCGCCTTATGG.*",a$completeNtSeq)) | a$aaLength!=8)
-  d <- subset(d,(!grepl(".TGTCTAGTACGCCTCTCTGCCTCTCTGCTAGTACGTGG.*",d$completeNtSeq)) | d$aaLength!=13)
-  d <- subset(d,(!grepl(".TGTGCTTCTGCCTTTCTGCCTGCTCAGGATTCTGCCTGCTCAGGAGCTCAGGATTCTCTGG.*",d$completeNtSeq)) | d$aaLength!=21)
-  d <- subset(d,(!grepl(".TGTGAGGAGTCCGTAGAGAGAGGAGTCCAGCGTAGCCATGCCTAAGGAGTCCCAGCCTCGGTAGAGAGAGCGCTGG.*",d$completeNtSeq)) | d$aaLength!=26)
-  d <- subset(d,(!grepl(".TGTAAGGAGTAACTGCATAACTGCATACTAAGCCTAAGGAGTATGG.*",d$completeNtSeq)) | d$aaLength!=16)
-  d <- subset(d,(!grepl(".TGTGTGCCTCTTTCCTCTACTAGATCGCCTCTCTATTATCCTCTAGAGTAGAGTAAGGAGTAGATCGCTATCCTCTGTAAGGAGTCCTCTACCTGG.*",d$completeNtSeq)) | d$aaLength!=32)
-  #d <- subset(d,(t$aaSeq!="noju"))
+  a <- datalist[[i]][,c("vGene","jGene","aaSeq","aaLength","size","completeNtSeq","vAndJchainSimplified")]
+  a <- subset(a,(t$aaSeq!="noju"))
+  a.all <- a
+  #============ check for templates =======
+  a$template<-'no'
+  #Akash‘s templates
+  a$template[grepl("TGTGCATCACGACACAGTGGTCTGG",a$completeNtSeq)]<-'t1'
+  a$template[grepl("TGTGCATCACGACCAGATCCACAGATCCATTGGTTACTGG",a$completeNtSeq)]<-'t2'
+  #Tamara‘s templates
+  a$template[grepl("TGTTCGCCTTATCGCCTTATGG",a$completeNtSeq)]<-'IGHV1-30_IGHJ4'
+  a$template[grepl("TGTCTAGTACGCCTCTCTGCCTCTCTGCTAGTACGTGG",a$completeNtSeq)]<-'IGHV1-30_IGHJ6'
+  a$template[grepl("TGTGCTTCTGCCTTTCTGCCTGCTCAGGATTCTGCCTGCTCAGGAGCTCAGGATTCTCTGG",a$completeNtSeq)]<-'IGHV3-1_IGHJ4'
+  a$template[grepl("TGTGAGGAGTCCGTAGAGAGAGGAGTCCAGCGTAGCCATGCCTAAGGAGTCCCAGCCTCGGTAGAGAGAGCGCTGG",a$completeNtSeq)]<-'IGHV3-1_IGHJ6'
+  a$template[grepl("TGTAAGGAGTAACTGCATAACTGCATACTAAGCCTAAGGAGTATGG",a$completeNtSeq)]<-'IGHV4-1_IGHJ4'
+  a$template[grepl("TGTGTGCCTCTTTCCTCTACTAGATCGCCTCTCTATTATCCTCTAGAGTAGAGTAAGGAGTAGATCGCTATCCTCTGTAAGGAGTCCTCTACCTGG",a$completeNtSeq)]<-'IGHV4-1_IGHJ6'
+  a.woTemp<-a[a$template=='no',]
+  a.tempOnly<-a[a$template!='no',]
+  
+  #============ summarize templates for sample and store in tibble =======
+  temp<-as_tibble(ddply(a.tempOnly[,c("template","size")],"template",numcolwise(sum)))
+  if(nrow(temp)==0){next}
+  temp$sample<-files_short[i]
+  templateSummary<-bind_rows(templateSummary,temp)
+ ####### make compared Plots between all reads AAseqLength VS Size and without template reads ######  
   plotlist<-list()
   for (j in 1:length(loci)){
     create.stackedbar<-function(t) {
@@ -84,8 +100,8 @@ for (i in 1:length(datalist)){
     p<-ggplot(tt,aes(length,size,fill=aaSeq))+geom_col(position = position_stack(reverse = FALSE))+guides(fill=F)+scale_fill_manual(values=colors)+xlim(1,35)+ggtitle(plotname)
     #plotlist[[j]]<-ggplotGrob(p)
     }
-    plot1<-create.stackedbar(a)
-    plot2<-create.stackedbar(d)
+    plot1<-create.stackedbar(a.all)
+    plot2<-create.stackedbar(a.woTemp)
     plotvs<-list(plot1,plot2)
     for (m in 1:length(plotvs)) {
       plotlist[[m]]<-ggplotGrob(plotvs[[m]])  
