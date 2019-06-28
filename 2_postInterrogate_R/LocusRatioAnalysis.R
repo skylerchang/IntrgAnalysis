a4ddd5a1ebff87f859230800f9ad50cb1b59e951
Tamara Morrill <tmorrill@uoguelph.ca>
2019-06-28 13:34:28 -0400

    R code for for looking at the locus ratios

---
+++ 2_postInterrogate_R/LocusRatioAnalysis.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(here)
+library(RColorBrewer)
+library(reshape2)
+library(gsubfn)
+library(gridExtra)
+
+
+#*************************** adjust settings ************************
+#Is SampleNo coding? values: T/F; (e.g. MRD - yes: jake-01.1 (pbmcs), jake-01.2 (plasma); Histiocytoma - no)
+sampleNoCoding<-1
+
+#********************************************************************
+setwd(here())
+getwd()
+
+targetDir<-'../Results/'
+
+files<-list.files(targetDir,pattern = "^locus.*xlsx")
+#pick which file to use
+# 1 = runs 21, 24, 26
+# 2 = run 28
+
+j<-1
+j<-2
+
+  L<-readxl::read_excel(paste0(targetDir,files[j]))
+  
+# adding the means 
+L$mean.IGH<-(L$IGH.rep1+L$IGH.rep2)/2
+L$mean.TRB<-(L$TRB.rep1+L$TRB.rep2)/2
+
+#calculate the ratio 
+L$locus.Ratio<-L$mean.IGH/L$mean.TRB
+
+#plot ratios agaisnt each other 
+#no log
+pdf("../Results/locus ratio.pdf")
+ggplot(L,aes(locus.Ratio,submission))+geom_point()
+dev.off()
+#log axis 
+pdf("../Results/locus ratio(log).pdf")
+ggplot(L,aes(locus.Ratio,submission))+geom_point()+scale_x_log10()
+dev.off()
