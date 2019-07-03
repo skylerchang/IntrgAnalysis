02820aba0b1daf338f352055a0dacc9a2be94a6f
Tamara Morrill <tmorrill@uoguelph.ca>
2019-07-03 17:30:51 -0400

    added the locus ratio analysis and created rds file

--- 2_postInterrogate_R/diversity.R
+++ 2_postInterrogate_R/diversity.R
@@ -283,3 +283,54 @@ dev.off()
 templateSummary
 ggplot(templateSummary,aes(template,size,fill=sample))+geom_col()+coord_flip()
 ggplot(templateSummary,aes(sample,size,fill=template))+geom_col()+coord_flip()
+
+
+#locus analysis 
+#*************************** adjust settings ************************
+sampleNoCoding<-1
+
+#********************************************************************
+setwd(here())
+getwd()
+
+targetDir<-'../Results/'
+
+files<-list.files(targetDir,pattern = "^locus.*xlsx")
+#pick which file to use
+# 1 = runs 21, 24, 26
+# 2 = run 28
+
+j<-1
+j<-2
+
+L<-readxl::read_excel(paste0(targetDir,files[j]))
+
+# adding the means 
+L$mean.IGH<-(L$IGH.rep1+L$IGH.rep2)/2
+L$mean.TRB<-(L$TRB.rep1+L$TRB.rep2)/2
+
+#calculate the ratio 
+L$locus.Ratio<-L$mean.IGH/L$mean.TRB
+
+#plot ratios agaisnt each other 
+#no log
+pdf("../Results/locus ratio.pdf")
+ggplot(L,aes(locus.Ratio,submission))+geom_point()
+dev.off()
+#log axis 
+pdf("../Results/locus ratio(log-axis).pdf")
+ggplot(L,aes(locus.Ratio,submission))+geom_point()+scale_x_log10()+ geom_vline(xintercept = 1)
+dev.off()
+#log transformation
+pdf("../Results/locus ratio(log).pdf")
+ggplot(L,aes(log(locus.Ratio),submission))+geom_point()+ geom_vline(xintercept = 0)
+dev.off()
+#summary
+summary(L$mean.IGH)
+summary(L$mean.TRB)
+summary(L$locus.Ratio)
+
+# save as RDS file 
+
+saveRDS(L, file = "LocusRatiodata.rds")
+
